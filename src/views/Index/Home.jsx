64c5f33472597d1ee9cb36eb42893eba041891c1
msm <1139630078@qq.com>
2020-04-28 00:35:39 +0800

    首页完成一半-4.28

--- src/views/Index/Home.jsx
+++ src/views/Index/Home.jsx
@@ -1,24 +1,127 @@
 import React, { Component } from "react";
-
+import style from "../../assets/css/home/home.module.css";
 class Home extends Component {
   render() {
     return (
-      <React.Fragment>
-        <div className="head">
-          首页
-          {/* <div className="head-address">
-            <img src="../img/home/location.png" alt="">
+      <div>
+        <div className={style["head"]}>
+          <div className={style["head-address"]}>
+            <img src={require("../../assets/img/location.png")} alt="" />
             <span>全国</span>
+          </div>
+          <div className={style["head-search"]}>
+            <img src={require("../../assets/img/search.png")} alt="" />
+            <input type="text" placeholder="搜索热门演出" />
+          </div>
+          <div className={style["head-calendar"]}>
+            <img src={require("../../assets/img/calendar.png")} alt="" />
+          </div>
         </div>
-        <div className="head-search">
-            <img src="../img//home/search.png" alt="">
-            <input type="text" placeholder="搜索热门演出">
+        <div class={style["adv"]}>
+          <img src={require("../../assets/img/adv.jpg")} alt="" />
         </div>
-        <div className="head-calendar">
-            <img src="../img//home/calendar.png" alt="">
-        </div>  */}
+        <div class={style["typedetaile"]}>
+          <ul>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+            <li>
+              <img src={require("../../assets/img/m.png")} alt="" />
+              <span>演唱会</span>
+            </li>
+          </ul>
         </div>
-      </React.Fragment>
+        {/* <!-- vip折扣 --> */}
+        <div className={style["vipscript"]}>
+          <div className={style["vipadv"]}>
+            <div className={style["vipadv-head"]}>
+              <div className={style["vip-left"]}>
+                <img src={require("../../assets/img/vipicon.png")} alt="" />
+                <span>会员专属折扣</span>
+              </div>
+              <div className={style["vip-right"]}>
+                <span>99元/年</span>
+                <img src={require("../../assets/img/arrow.png")} alt="" />
+              </div>
+              <div className={style["vipdiscount-line"]}></div>
+            </div>
+            <div className={style["viplb"]}>
+              <div className={style["viplb-left"]}>
+                <img src={require("../../assets/img/books.jpg")} alt="" />
+              </div>
+              <div className={style["viplb-right"]}>
+                <p>浪漫经典童话剧《美女与野兽》</p>
+                <div>
+                  <span className={style["discount"]}>
+                    <mark className={style["markk"]}>5</mark>折起
+                  </span>
+                  <span className={style["panicbuing"]}>立即抢购</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={style["advbottom"]}>
+          <p>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </p>
+        </div>
+
+        {/* <!-- vip广告 --> */}
+        <div className={style["vipmsg"]}>
+          <img src={require("../../assets/img/vipmsg.png")} alt="" />
+        </div>
+
+        {/* <!-- 热门演出 --> */}
+        <div className={style["hotshow"]}>
+          <div className={style["hotshowTitle"]}>
+            <p className={style["hot"]}>热门演出</p>
+            <p className={style["hotAll"]}>
+              <span>全部</span>
+              <span className={style["hotlink"]}>
+                <img src={require("../../assets/img/arrow.png")} alt="" />
+              </span>
+            </p>
+          </div>
+          <div className={style["hotshowlist"]}></div>
+        </div>
+      </div>
     );
   }
 }
--- src/views/Index/index.jsx
+++ src/views/Index/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import style from "../../assets/css/index/index.module.css";
 import MyRouter from "../../components/common/MyRouter";
-
 class index extends Component {
   constructor(props) {
     super(props);
@@ -10,23 +10,29 @@ class index extends Component {
     return (
       <div>
         <MyRouter router={this.props.children}></MyRouter>
-        <nav style={style.navfoot}>
-          <NavLink to={"/"}>首页</NavLink>
-          <NavLink to={"/TheatreList"}>剧院</NavLink>
-          <NavLink to={"/Eticket"}>票夹</NavLink>
-          <NavLink to={"/Myjuooo"}>我的</NavLink>
-        </nav>
+        <div className={style.foot}>
+          <nav className={style.navfoot}>
+            <div className={style.footCommon}>
+              <img src={require("../../assets/img/home.png")} alt="" />
+              <NavLink to={"/"}>首页</NavLink>
+            </div>
+            <div className={style.footCommon}>
+              <img src={require("../../assets/img/eticket.png")} alt="" />
+              <NavLink to={"/TheatreList"}>剧院</NavLink>
+            </div>
+            <div className={style.footCommon}>
+              <img src={require("../../assets/img/eticket.png")} alt="" />
+              <NavLink to={"/Eticket"}>票夹</NavLink>
+            </div>
+            <div className={style.footCommon}>
+              <img src={require("../../assets/img/my.png")} alt="" />
+              <NavLink to={"/Myjuooo"}>我的</NavLink>
+            </div>
+          </nav>
+        </div>
       </div>
     );
   }
 }
 
 export default index;
-
-const style = {
-  navfoot: {
-    fontSize: " 0.56667rem",
-    display: "flex",
-    justifyContent: "space-between",
-  },
-};
